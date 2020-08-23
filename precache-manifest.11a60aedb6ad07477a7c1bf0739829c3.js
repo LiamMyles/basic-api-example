dbbf200f365d8580804c343e162bfb0b39a77f95
Liam Myles <liamthemonkey92@gmail.com>
2020-08-23 18:44:37 +0100

    Updates

---
+++ precache-manifest.11a60aedb6ad07477a7c1bf0739829c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ce7a7247cce274a69df66e912dae8a",
+    "url": "/basic-api-example/index.html"
+  },
+  {
+    "revision": "a31bf215ecf22ccb55bd",
+    "url": "/basic-api-example/static/css/main.54f9320b.chunk.css"
+  },
+  {
+    "revision": "7415ffa2e7090891a1a9",
+    "url": "/basic-api-example/static/js/2.94116b25.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/basic-api-example/static/js/2.94116b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a31bf215ecf22ccb55bd",
+    "url": "/basic-api-example/static/js/main.c355ba16.chunk.js"
+  },
+  {
+    "revision": "2ccd3b60a0f5de1989fa",
+    "url": "/basic-api-example/static/js/runtime-main.44f3f9e3.js"
+  },
+  {
+    "revision": "1c34e3c05b20f8c75e11de5e207cda48",
+    "url": "/basic-api-example/static/media/beer-icon-large.1c34e3c0.png"
+  },
+  {
+    "revision": "225e6d9cdaf36ea5ae2195cd55977fc7",
+    "url": "/basic-api-example/static/media/milkshake-icon.225e6d9c.png"
+  }
+]);
